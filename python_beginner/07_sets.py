8937ad10177614c8c718f6df7d368c3f15413780
mridul162 <mridulbd75@gmail.com>
2025-11-11 15:54:53 +0600

    update set.py

--- python_beginner/07_sets.py
+++ python_beginner/07_sets.py
@@ -19,49 +19,66 @@ print(type(potter))
 print("\n4. Iterating through a set:")
 for value in info:
     print(value)
-###########################
 
-# s1 = {1, 2, 5, 6}
-# s2 = {2, 4, 5, 7}
+# 5. Set operations:
+print("\n5. Set operations:")
 
-# print(s1.union(s2))
-# s1.update(s2)
-# print(s1)
+s1 = {1, 2, 5, 6}
+s2 = {2, 4, 5, 7}
+print("   a. s1 Union s2: ", s1.union(s2))  
+s1.update(s2)                                                      # Updates s1 to union
+print("   b. Updates s1 to union: s1=", s1)
 
-# s_int = s1.intersection(s2)
-# print(s_int)
-# s1.intersection_update(s2)
-# print(s1)
+s1 = {1, 2, 5, 6}
+s_int = s1.intersection(s2) 
+print("   c. s1 intersection s2: ", s_int)
+s1.intersection_update(s2)                                         # Updates s1 to intersection
+print("   d. Updates s1 to intersection: s1=", s1)
 
-# s_sd = s1.symmetric_difference(s2)
-# print(s_sd)
-# s1.symmetric_difference_update(s2)
-# print(s1)
+s1 = {1, 2, 5, 6}
+s_sd = s1.symmetric_difference(s2)                         
+print("   e. Symmetric difference (s1-s2): ", s_sd)
+s1.symmetric_difference_update(s2)                                 # Updates s1 to symmetric difference
+print("   f. Updates s1 to symmetric difference: s1=", s1)
 
-# print(s1.isdisjoint(s2))
-# print(s1.issuperset(s2))
-# print(s1.issubset(s2))
-# s1.add(7)
-# s1.add(4)
-# print(s1)
-# print(s1.issuperset(s2))
-# print(s2.issubset(s1))
+s1 = {1, 2, 5, 6}
+print("   g. s1 and s2 have no elements in common: ", s1.isdisjoint(s2))  # True if s1 and s2 have no elements in common
+print("   h. s1 contains all elements of s2: ", s1.issuperset(s2))        # True if s1 contains all elements of s2
+print("   i. s1 is contained in s2: ", s1.issubset(s2))                   # True if s1 is contained in s2
+ 
+s1.add(7)
+s1.add(4)
+print("   Now s1=", s1)
 
-# s1.remove(9)
-# print(s1)
-# s1.discard(9)
-# print(s1)
+print("   j. s1 contains all elements of s2: ", s1.issuperset(s2)) 
+print("   k. s1 is contained in s2: ", s2.issubset(s1))  
 
-# s1.pop()
-# s1.pop()
-# s1.pop()
-# print(s1)
+# 6. Set methods:
+print("\n6. Set methods:")
 
-# del s1
-# print(s1)
+s1 = {1, 2, 5, 6}
+# s1.remove(9)       # This will raise an error if 9 is not present
+print(s1)
 
-# s1.clear()
-# print(s1)
+s1.discard(9)        # This will not raise an error if 9 is not present
+print(s1)
 
-# print(1 in s1)
+print(s1.pop())      # Removes and returns an arbitrary element
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
+
+s1 = {1, 2, 5, 6}
+# del s1             # Deletes the set entirely
+print(s1)            # This will raise an error since s1 is deleted
+
+s1.clear()           # Removes all elements from the set
+print(s1)            # Should print an empty set
+
+s1 = {1, 2, 5, 6}
+print(1 in s1)
 
